c75fcb2510f78630ee67c9c856d922b9822c628f
parry <331940353@qq.com>
2018-08-24 15:15:46 +0800

    android 工具类

---
+++ androidToolsUtils/src/main/java/com/parry/utils/code/CloseUtils.java
@@ -0,0 +1,53 @@
+package com.parry.utils.code;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * <pre>
+ *     author: parry
+ *     time  : 2016/09/21
+ *     desc  : utils about log
+ * </pre>
+ */
+public final class CloseUtils {
+
+    private CloseUtils() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
+    }
+
+    /**
+     * Close the io stream.
+     *
+     * @param closeables closeables
+     */
+    public static void closeIO(final Closeable... closeables) {
+        if (closeables == null) return;
+        for (Closeable closeable : closeables) {
+            if (closeable != null) {
+                try {
+                    closeable.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * Close the io stream quietly.
+     *
+     * @param closeables closeables
+     */
+    public static void closeIOQuietly(final Closeable... closeables) {
+        if (closeables == null) return;
+        for (Closeable closeable : closeables) {
+            if (closeable != null) {
+                try {
+                    closeable.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
+    }
+}
---
+++ androidToolsUtils/src/main/java/com/parry/utils/constant/MemoryConstants.java
@@ -0,0 +1,26 @@
+package com.parry.utils.constant;
+
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * <pre>
+ *     author: parry
+ *     time  : 2018.08.24
+ *     desc  : utils about initialization
+ * </pre>
+ */
+public final class MemoryConstants {
+
+    public static final int BYTE = 1;
+    public static final int KB   = 1024;
+    public static final int MB   = 1048576;
+    public static final int GB   = 1073741824;
+
+    @IntDef({BYTE, KB, MB, GB})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Unit {
+    }
+}
